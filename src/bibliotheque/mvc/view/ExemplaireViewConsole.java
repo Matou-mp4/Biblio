12c1a558ee6ddabc300560140e7e55d997618a48
Matou <antoine.dangreau@condorcet.be>
2024-04-16 17:17:34 +0200

    modif des viewConsole

--- src/bibliotheque/mvc/view/ExemplaireViewConsole.java
+++ src/bibliotheque/mvc/view/ExemplaireViewConsole.java
@@ -1,15 +1,167 @@
 package bibliotheque.mvc.view;
 
+import bibliotheque.metier.Exemplaire;
+import bibliotheque.metier.TypeLivre;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+
+import static bibliotheque.utilitaires.Utilitaire.*;
+
 
 public class ExemplaireViewConsole extends AbstractViewExemplaire {
+    Scanner sc = new Scanner(System.in);
+
+
     @Override
     public void menu() {
+        update(ExemplaireController.getAll());
+        List options = Arrays.asList("ajouter", "retirer", "rechercher","modifier","fin");
+        do {
+            int ch = choixListe(options);
+
+            switch (ch) {
+                case 1:
+                    ajouter();
+                    break;
+                case 2:
+                    retirer();
+                    break;
+                case 3:
+                    rechercher();
+                    break;
+                case 4:
+                    modifier();
+                    break;
+                case 5:
+                    return;
+            }
+        } while (true);
+    }
+
+    private void retirer() {
+        int nl = choixElt(la)-1;
+        Exemplaire a = la.get(nl);
+        boolean ok = ExemplaireController.remove(a);
+        if(ok) affMsg("Exemplaire effacé");
+        else affMsg("Exemplaire non effacé");
+    }
+
+    private void affMsg(String msg) {
+        System.out.println(msg);
+    }
+
+
+    public void rechercher() {
+        try {
+            System.out.println(" ");
+            String nom = sc.nextLine();
+            System.out.println("prénom ");
+            String prenom = sc.nextLine();
+            System.out.println("nationalité");
+            String nat = sc.nextLine();
+            Exemplaire rech = new Exemplaire(nom, prenom, nat);
+            Exemplaire a = ExemplaireController.search(rech);
+            if(a==null) affMsg("Exemplaire inconnu");
+            else {
+                affMsg(a.toString());
+                //special(a);
+            }
+        }catch(Exception e){
+            System.out.println("erreur : "+e);
+        }
 
     }
+
+
+    public void modifier() {
+        int choix = choixElt(la);
+        Exemplaire a = la.get(choix-1);
+        do {
+            try {
+                long code = sc.nextLong();
+                byte nbrePlages = sc.nextByte();
+                //LocalTime dureeTotale =sc.nextLine();
+                a.setCode(code);
+                a.setNbrePlages(nbrePlages);
+                //a.setDureeTotale(dureeTotale);
+                break;
+            } catch (Exception e) {
+                System.out.println("erreur :" + e);
+            }
+        }while(true);
+        ExemplaireController.update(a);
+    }
+
+
+    public void ajouter() {
+        Exemplaire a;
+        do {
+            try {
+                System.out.println("nom ");
+                String nom = sc.nextLine();
+                System.out.println("prénom ");
+                String prenom = sc.nextLine();
+                System.out.println("nationalité");
+                String nat = sc.nextLine();
+                a = new Exemplaire(nom, prenom, nat);
+                break;
+            } catch (Exception e) {
+                System.out.println("une erreur est survenue : "+e.getMessage());
+            }
+        }while(true);
+        ExemplaireController.add(a);
+    }
+
+//    public void special(Exemplaire a) {
+//
+//        List options = Arrays.asList("lister ouvrages", "lister livres", "lister par genre","fin");
+//        do {
+//            int ch = choixListe(options);
+//
+//            switch (ch) {
+//
+//                case 1:
+//                    listerOuvrages(a);
+//                    break;
+//                case 2:
+//                    listerLivres(a);
+//                    break;
+//                case 3:
+//                    listerGenre(a);
+//                    break;
+//                case 4 :return;
+//            }
+//        } while (true);
+//
+//    }
+//
+//
+////    public void listerGenre(Exemplaire a) {
+////        System.out.println("genre :");
+////        String genre = sc.nextLine();
+////        affListe(new ArrayList(ExemplaireController.listerOuvrages(a,genre)));
+////    }
+//
+//
+//    public void listerOuvrages(Exemplaire a){
+//        affList(new ArrayList(ExemplaireController.listerOuvrages(a)));
+//    }
+//
+//
+//    public void listerLivres(Exemplaire a){
+//        TypeLivre[] tlv = TypeLivre.values();
+//        int ch2 = choixListe(List.of(tlv));
+//        TypeLivre tl = tlv[ch2-1];
+//        affList(new ArrayList(ExemplaireController.listerLivre(a,tl)));
+//    }
 
     @Override
-    public void affList(List lex) {
-
+    public void affList(List la) {
+        affListe(la);
     }
 }
+
--- src/bibliotheque/mvc/view/MailViewConsole.java
+++ src/bibliotheque/mvc/view/MailViewConsole.java
@@ -1,4 +1,167 @@
 package bibliotheque.mvc.view;
 
-public class MailViewConsole {
+import bibliotheque.metier.Mail;
+import bibliotheque.metier.TypeLivre;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import static bibliotheque.utilitaires.Utilitaire.*;
+
+
+public class MailViewConsole extends AbstractViewMail {
+    Scanner sc = new Scanner(System.in);
+
+
+    @Override
+    public void menu() {
+        update(MailController.getAll());
+        List options = Arrays.asList("ajouter", "retirer", "rechercher","modifier","fin");
+        do {
+            int ch = choixListe(options);
+
+            switch (ch) {
+                case 1:
+                    ajouter();
+                    break;
+                case 2:
+                    retirer();
+                    break;
+                case 3:
+                    rechercher();
+                    break;
+                case 4:
+                    modifier();
+                    break;
+                case 5:
+                    return;
+            }
+        } while (true);
+    }
+
+    private void retirer() {
+        int nl = choixElt(la)-1;
+        Mail a = la.get(nl);
+        boolean ok = MailController.remove(a);
+        if(ok) affMsg("Mail effacé");
+        else affMsg("Mail non effacé");
+    }
+
+    private void affMsg(String msg) {
+        System.out.println(msg);
+    }
+
+
+    public void rechercher() {
+        try {
+            System.out.println(" ");
+            String nom = sc.nextLine();
+            System.out.println("prénom ");
+            String prenom = sc.nextLine();
+            System.out.println("nationalité");
+            String nat = sc.nextLine();
+            Mail rech = new Mail(nom, prenom, nat);
+            Mail a = MailController.search(rech);
+            if(a==null) affMsg("Mail inconnu");
+            else {
+                affMsg(a.toString());
+                //special(a);
+            }
+        }catch(Exception e){
+            System.out.println("erreur : "+e);
+        }
+
+    }
+
+
+    public void modifier() {
+        int choix = choixElt(la);
+        Mail a = la.get(choix-1);
+        do {
+            try {
+                long code = sc.nextLong();
+                byte nbrePlages = sc.nextByte();
+                //LocalTime dureeTotale =sc.nextLine();
+                a.setCode(code);
+                a.setNbrePlages(nbrePlages);
+                //a.setDureeTotale(dureeTotale);
+                break;
+            } catch (Exception e) {
+                System.out.println("erreur :" + e);
+            }
+        }while(true);
+        MailController.update(a);
+    }
+
+
+    public void ajouter() {
+        Mail a;
+        do {
+            try {
+                System.out.println("nom ");
+                String nom = sc.nextLine();
+                System.out.println("prénom ");
+                String prenom = sc.nextLine();
+                System.out.println("nationalité");
+                String nat = sc.nextLine();
+                a = new Mail(nom, prenom, nat);
+                break;
+            } catch (Exception e) {
+                System.out.println("une erreur est survenue : "+e.getMessage());
+            }
+        }while(true);
+        MailController.add(a);
+    }
+
+//    public void special(Mail a) {
+//
+//        List options = Arrays.asList("lister ouvrages", "lister livres", "lister par genre","fin");
+//        do {
+//            int ch = choixListe(options);
+//
+//            switch (ch) {
+//
+//                case 1:
+//                    listerOuvrages(a);
+//                    break;
+//                case 2:
+//                    listerLivres(a);
+//                    break;
+//                case 3:
+//                    listerGenre(a);
+//                    break;
+//                case 4 :return;
+//            }
+//        } while (true);
+//
+//    }
+//
+//
+////    public void listerGenre(Mail a) {
+////        System.out.println("genre :");
+////        String genre = sc.nextLine();
+////        affListe(new ArrayList(MailController.listerOuvrages(a,genre)));
+////    }
+//
+//
+//    public void listerOuvrages(Mail a){
+//        affList(new ArrayList(MailController.listerOuvrages(a)));
+//    }
+//
+//
+//    public void listerLivres(Mail a){
+//        TypeLivre[] tlv = TypeLivre.values();
+//        int ch2 = choixListe(List.of(tlv));
+//        TypeLivre tl = tlv[ch2-1];
+//        affList(new ArrayList(MailController.listerLivre(a,tl)));
+//    }
+
+    @Override
+    public void affList(List la) {
+        affListe(la);
+    }
 }
+
