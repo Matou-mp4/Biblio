bae3cf76e1e5551f56bad3d2afbedcdd317949fc
Matou <antoine.dangreau@condorcet.be>
2024-04-16 16:45:48 +0200

    modif des model

--- src/bibliotheque/mvc/model/ExemplaireModel.java
+++ src/bibliotheque/mvc/model/ExemplaireModel.java
@@ -1,33 +1,69 @@
 package bibliotheque.mvc.model;
 
 import bibliotheque.metier.Exemplaire;
+import bibliotheque.metier.Livre;
+import bibliotheque.metier.Ouvrage;
+import bibliotheque.metier.TypeLivre;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
-public class ExemplaireModel extends DAOExemplaire{
+public class ExemplaireModel extends DAOExemplaire {
+
+    private List<Exemplaire> ldatas = new ArrayList<>();
+
 
     @Override
-    public Exemplaire add(Exemplaire elt) {
-        return null;
+    public Exemplaire add( Exemplaire elt) {
+        boolean present =ldatas.contains(elt);
+        if (!present) {
+            ldatas.add(elt);
+            notifyObservers();
+            return elt;
+        } else return null;
     }
 
     @Override
-    public boolean remove(Exemplaire elt) {
-        return false;
+    public boolean remove( Exemplaire elt) {
+        boolean ok = ldatas.remove(elt);
+        notifyObservers();
+        return ok;
     }
 
     @Override
     public Exemplaire update(Exemplaire elt) {
-        return null;
+        int p = ldatas.indexOf(elt);
+        if (p < 0) return null;
+        ldatas.set(p, elt);
+        notifyObservers();
+        return elt;
     }
 
     @Override
     public Exemplaire read(Exemplaire rech) {
-        return null;
+        int p = ldatas.indexOf(rech);
+        if(p<0) return null;
+        return ldatas.get(p);
     }
 
     @Override
     public List<Exemplaire> getAll() {
-        return null;
+        return ldatas;
+    }
+
+    @Override
+    public Set<Ouvrage> listerOuvrages(Exemplaire a) {
+        return a.listerOuvrages();
+    }
+
+    @Override
+    public List<Livre> listerLivre(Exemplaire a, TypeLivre tl) {
+        return a.listerLivres(tl);
+    }
+
+    @Override
+    public List<Ouvrage> listerOuvrages(Exemplaire a, String genre) {
+        return a.listerOuvrages(genre);
     }
 }
--- src/bibliotheque/mvc/model/MailModel.java
+++ src/bibliotheque/mvc/model/MailModel.java
@@ -1,4 +1,69 @@
 package bibliotheque.mvc.model;
 
-public class MailModel {
+import bibliotheque.metier.Mail;
+import bibliotheque.metier.Livre;
+import bibliotheque.metier.Ouvrage;
+import bibliotheque.metier.TypeLivre;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class MailModel extends DAOMail {
+
+    private List<Mail> ldatas = new ArrayList<>();
+
+
+    @Override
+    public Mail add( Mail elt) {
+        boolean present =ldatas.contains(elt);
+        if (!present) {
+            ldatas.add(elt);
+            notifyObservers();
+            return elt;
+        } else return null;
+    }
+
+    @Override
+    public boolean remove( Mail elt) {
+        boolean ok = ldatas.remove(elt);
+        notifyObservers();
+        return ok;
+    }
+
+    @Override
+    public Mail update(Mail elt) {
+        int p = ldatas.indexOf(elt);
+        if (p < 0) return null;
+        ldatas.set(p, elt);
+        notifyObservers();
+        return elt;
+    }
+
+    @Override
+    public Mail read(Mail rech) {
+        int p = ldatas.indexOf(rech);
+        if(p<0) return null;
+        return ldatas.get(p);
+    }
+
+    @Override
+    public List<Mail> getAll() {
+        return ldatas;
+    }
+
+    @Override
+    public Set<Ouvrage> listerOuvrages(Mail a) {
+        return a.listerOuvrages();
+    }
+
+    @Override
+    public List<Livre> listerLivre(Mail a, TypeLivre tl) {
+        return a.listerLivres(tl);
+    }
+
+    @Override
+    public List<Ouvrage> listerOuvrages(Mail a, String genre) {
+        return a.listerOuvrages(genre);
+    }
 }
